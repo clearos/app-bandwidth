ed5ab079e36ad317a56e92710f5d21980e14a80c
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-01-24 19:08:28 +0000

    - Disable advanced rules for 6.4.0

--- controllers/bandwidth.php
+++ controllers/bandwidth.php
@@ -63,7 +63,8 @@ class Bandwidth extends ClearOS_Controller
         // Load views
         //-----------
 
-        $views = array('bandwidth/server', 'bandwidth/ifaces', 'bandwidth/basic', 'bandwidth/advanced');
+        // $views = array('bandwidth/server', 'bandwidth/ifaces', 'bandwidth/basic', 'bandwidth/advanced');
+        $views = array('bandwidth/server', 'bandwidth/ifaces', 'bandwidth/basic');
 
         $this->page->view_forms($views, lang('bandwidth_app_name'));
     }
