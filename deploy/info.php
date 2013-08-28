9541e1d84442f6ae71e769b5b194d0659449b549
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-28 13:55:30 +0000

    - Prepped for 6.5.0 Beta 1 release

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'bandwidth';
-$app['version'] = '1.4.37';
+$app['version'] = '1.5.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-bandwidth.spec
+++ packaging/app-bandwidth.spec
@@ -1,7 +1,7 @@
 
 Name: app-bandwidth
 Epoch: 1
-Version: 1.4.37
+Version: 1.5.0
 Release: 1%{dist}
 Summary: Bandwidth Manager
 License: GPLv3
